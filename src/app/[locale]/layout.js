16019a341fac97a8279a762926c254e6adebd6d0
vinod <vinodkumarmahato1@gmail.com>
2024-08-18 12:13:59 +0530

    next update

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Icons, ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.min.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
