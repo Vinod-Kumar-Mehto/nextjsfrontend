ebc3bc946832f2655990da5c5bf12ab0c6bd6ba4
vinod <vinodkumarmahato1@gmail.com>
2024-09-15 15:46:05 +0530

    horizon website

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -12,6 +12,13 @@ export default async function RootLayout({ children, params }) {
   const messages = await getMessages();
   return (
     <html lang={params.locale}>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8040030284338511"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>
         <NextIntlClientProvider messages={messages}>
           <Header />
